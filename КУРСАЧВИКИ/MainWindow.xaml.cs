fa5796b646aaf901868486086f250fc70ae2650c
79138 <79138@DESKTOP-U5E4ODE>
2021-11-29 20:27:13 +0700

    Добавьте файлы проекта.

---
+++ КУРСАЧВИКИ/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace КУРСАЧВИКИ
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonAvtorise_Click(object sender, RoutedEventArgs e)
+        {
+            Avtorise avtorise = new Avtorise();
+            avtorise.Show();
+            Close();
+        }
+
+        private void ButtonRegistr_Click(object sender, RoutedEventArgs e)
+        {
+            Registr registr = new Registr();
+            registr.Show();
+            Close ();
+
+        }
+    }
+}
---
+++ КУРСАЧВИКИ/Registr.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace КУРСАЧВИКИ
+{
+    /// <summary>
+    /// Логика взаимодействия для Registr.xaml
+    /// </summary>
+    public partial class Registr : Window
+    {
+        public Registr()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBack_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            Close();
+        }
+
+        private void ButtonNext_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
